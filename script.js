7e514fee54ca2031e8c01df4a51fc70e2390aa02
nasmohd40@gmail.com <nasmohd40@gmail.com>
2022-10-02 22:31:59 +0300

    changes and tests

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ downloadBtn.addEventListener("click", e => {
     fetchFile(fileInput.value);
 });
 
+// alert (fileInput.value);
+alert ("https://cors-anywhere.herokuapp.com/" + fileInput.value);
+
 function fetchFile(url) {
-    fetch(url, {mode: "no-cors"}).then(res => res.blob()).then(file => {
+    fetch("https://circumvent-cors.herokuapp.com/" + url).then(res => res.blob()).then(file => {
         let tempUrl = URL.createObjectURL(file);
         const aTag = document.createElement("a");
         aTag.href = tempUrl;
@@ -19,10 +22,22 @@ function fetchFile(url) {
         URL.revokeObjectURL(tempUrl);
         aTag.remove();
 
-        
+
     }).catch(() => {
         alert("Failed to download file!");
         downloadBtn.innerText = "Download File";
     });
 }
+
+
+// var http_request;
+// http_request = new XMLHTTPRequest();
+// http_request.onreadystatechange = function () { /* .. */ };
+// http_request.open("POST", "https://sso.moxio.com");
+// http_request.withCredentials = true;
+// http_request.setRequestHeader("Content-Type", "application/json");
+// http_request.send({ 'request': "authentication token" });
+// Trying to circumvent this with {mode: 'no-cors'} does not work as one might 
+// expect: although 'no-cors' will ignore the CORS headers of the server the response 
+// content will then be inaccessible to your javascript code
 
